4958b367699c74d108030a7013d9bdf57d90aff5
Hejow <gmlwh124@naver.com>
2023-10-23 22:30:23 +0900

    Init: project

---
+++ src/main/java/com/newbarams/ajaja/AjajaApplication.java
@@ -0,0 +1,11 @@
+package com.newbarams.ajaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjajaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AjajaApplication.class, args);
+    }
+}
