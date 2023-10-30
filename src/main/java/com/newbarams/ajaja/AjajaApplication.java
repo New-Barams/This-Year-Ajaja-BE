d17fb65738e14fcaa919f4f54be3f8bb96ac6926
Hejow <gmlwh124@naver.com>
2023-10-30 23:13:53 +0900

    [AJA-166] Add: CD flows

--- src/main/java/com/newbarams/ajaja/AjajaApplication.java
+++ src/main/java/com/newbarams/ajaja/AjajaApplication.java
@@ -2,10 +2,17 @@ package com.newbarams.ajaja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
 public class AjajaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AjajaApplication.class, args);
+	}
+
+	@PostMapping("/health-check")
+	public ResponseEntity<Void> healthCheck() {
+		return ResponseEntity.noContent().build();
 	}
 }
