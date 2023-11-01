cb269b53a26b8f1c4c0ee792fe6686b24a085fc4
2jie0516 <dhtpgks10044@gmail.com>
2023-11-01 21:54:47 +0900

    Add(.src):리마인드 목 데이터 컨트롤러 추가

---
+++ src/main/java/com/newbarams/ajaja/module/remind/RemindController.java
@@ -0,0 +1,42 @@
+package com.newbarams.ajaja.module.remind;
+
+import static org.springframework.http.HttpStatus.*;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.newbarams.ajaja.global.common.AjajaResponse;
+import com.newbarams.ajaja.module.remind.domain.dto.GetReminds;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "remind", description = "리마인드 API")
+@RestController
+@RequestMapping("/reminds")
+public class RemindController {
+
+	@Tag(name = "remind")
+	@Operation(description = "[테스트] 리마인드 정보 조회 API", responses = {
+		@ApiResponse(responseCode = "200", description = "리마인드 정보들을 조회")
+	})
+	@GetMapping("/list")
+	@ResponseStatus(OK)
+	public AjajaResponse<GetReminds.CommonResponse> getReminds(
+
+	) {
+		List<GetReminds.Response> responses = List.of(
+			new GetReminds.Response("화이팅", true, 60, Timestamp.valueOf("2023-4-02 23:59:59")),
+			new GetReminds.Response("아좌좌", true, 60, Timestamp.valueOf("2023-7-02 23:59:59")),
+			new GetReminds.Response("할수있다", false, 0, Timestamp.valueOf("2023-10-02 23:59:59")));
+
+		GetReminds.CommonResponse commonResponse = new GetReminds.CommonResponse("moring", 2, 3, 1, true, responses);
+		return AjajaResponse.ok(commonResponse);
+	}
+}
