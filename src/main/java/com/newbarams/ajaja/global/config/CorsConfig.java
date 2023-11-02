e3d476487616808b0792353f769f476feb2e6fbb
Hejow <gmlwh124@naver.com>
2023-11-02 13:41:25 -1000

    Add(config): mock cors config

---
+++ src/main/java/com/newbarams/ajaja/global/config/CorsConfig.java
@@ -0,0 +1,48 @@
+package com.newbarams.ajaja.global.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+	private static final String FRONT_LOCAL_ENV = "http://localhost:3000/";
+
+	private final List<String> allowedMethods = List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		source.registerCorsConfiguration("/**", defaultCorsConfig());
+		source.registerCorsConfiguration("/mock/**", mockCorsConfig());
+		
+		return source;
+	}
+
+	private CorsConfiguration defaultCorsConfig() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(List.of(FRONT_LOCAL_ENV)); // will update
+		configuration.setAllowedMethods(allowedMethods);
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
+
+		return configuration;
+	}
+
+	private CorsConfiguration mockCorsConfig() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(List.of(FRONT_LOCAL_ENV));
+		configuration.setAllowedMethods(allowedMethods);
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
+
+		return configuration;
+	}
+}
--- src/main/java/com/newbarams/ajaja/global/config/SecurityConfig.java
+++ src/main/java/com/newbarams/ajaja/global/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.newbarams.ajaja.global.config;
 
-import java.util.List;
+import static org.springframework.security.config.Customizer.*;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -24,25 +21,11 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-
-		configuration.setAllowedOrigins(List.of("http://localhost:3000/"));
-		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-		configuration.addAllowedHeader("*");
-		configuration.setAllowCredentials(true);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
-
-	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.csrf(AbstractHttpConfigurer::disable)
-			.cors(httpSecurityCorsConfigurer -> corsConfigurationSource())
+			.cors(withDefaults())
 			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
