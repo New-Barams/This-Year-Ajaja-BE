08aa6630b379a8d634d387aeb93ac50c4258343d
Hejow <gmlwh124@naver.com>
2023-11-03 03:54:41 -1000

    [AJA-300] Feat(global): querydsl config

---
+++ src/main/java/com/newbarams/ajaja/global/config/QuerydslConfig.java
@@ -0,0 +1,22 @@
+package com.newbarams.ajaja.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Configuration
+@EnableJpaAuditing
+public class QuerydslConfig {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
+}
