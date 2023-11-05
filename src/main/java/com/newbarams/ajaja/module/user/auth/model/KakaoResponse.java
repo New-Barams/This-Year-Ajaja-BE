2e2cecb9e41f4be77a2c3d4f5cc326581eea1867
Hejow <gmlwh124@naver.com>
2023-11-05 01:21:42 -1000

    [AJA-268] Add(auth): kakao login setup

---
+++ src/main/java/com/newbarams/ajaja/module/user/auth/model/KakaoResponse.java
@@ -0,0 +1,26 @@
+package com.newbarams.ajaja.module.user.auth.model;
+
+import static com.newbarams.ajaja.module.user.auth.model.KakaoResponse.*;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+public sealed interface KakaoResponse permits Token, Profile {
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	record Token(
+		String accessToken,
+		String tokenType,
+		String refreshToken,
+		int expiresIn,
+		String scope,
+		String refreshTokenExpiresIn
+	) implements KakaoResponse {
+	}
+
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	record Profile(
+		Long id,
+		KakaoAccount kakaoAccount
+	) implements KakaoResponse {
+	}
+}
