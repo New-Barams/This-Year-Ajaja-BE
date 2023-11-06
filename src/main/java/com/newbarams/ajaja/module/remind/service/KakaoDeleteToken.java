23fbec14d24fda53b708ac759e12519544afe4f2
2jie0516 <dhtpgks10044@gmail.com>
2023-11-06 13:29:30 +0900

    [AJA-279] Add(.src):카카오 리마인드 인터페이스 분리

---
+++ src/main/java/com/newbarams/ajaja/module/remind/service/KakaoDeleteToken.java
@@ -0,0 +1,8 @@
+package com.newbarams.ajaja.module.remind.service;
+
+/**
+ * 토큰 삭제 API
+ */
+public interface KakaoDeleteToken {
+	void deletePushToken();
+}
---
+++ src/main/java/com/newbarams/ajaja/module/remind/service/KakaoRegisterToken.java
@@ -0,0 +1,8 @@
+package com.newbarams.ajaja.module.remind.service;
+
+/**
+ * 토큰 등록 인터페이스
+ */
+public interface KakaoRegisterToken {
+	void registerPushToken();
+}
