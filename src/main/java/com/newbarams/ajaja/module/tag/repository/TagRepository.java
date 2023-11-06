242369b3066f4368fb717f5f1dc89c3ce43eda19
kys0411 <32180846@dankook.ac.kr>
2023-11-07 01:29:34 +0900

    [AJA-302] Chore: @Repository 제거

--- src/main/java/com/newbarams/ajaja/module/tag/repository/TagRepository.java
+++ src/main/java/com/newbarams/ajaja/module/tag/repository/TagRepository.java
@@ -3,11 +3,9 @@ package com.newbarams.ajaja.module.tag.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.newbarams.ajaja.module.tag.domain.Tag;
 
-@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 	Optional<Tag> findByName(String name);
 }
