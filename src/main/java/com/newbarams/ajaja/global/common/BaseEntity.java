75f374468ebe5ba2b1753aa9637723f8a9a115cc
kys0411 <32180846@dankook.ac.kr>
2023-11-06 23:55:03 +0900

    [AJA-302] Add: getter 추가

--- src/main/java/com/newbarams/ajaja/global/common/BaseEntity.java
+++ src/main/java/com/newbarams/ajaja/global/common/BaseEntity.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity<T> extends SelfValidating<T> {
