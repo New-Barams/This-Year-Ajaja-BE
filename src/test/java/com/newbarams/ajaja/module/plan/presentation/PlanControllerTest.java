095ae244e4cdcb0e6d3341bfc71ffec5ae0900bc
2jie0516 <dhtpgks10044@gmail.com>
2023-11-08 18:00:54 +0900

    [AJA-308] Test(.src):컨트롤러 테스트 추가

---
+++ src/test/java/com/newbarams/ajaja/module/plan/presentation/PlanControllerTest.java
@@ -0,0 +1,43 @@
+package com.newbarams.ajaja.module.plan.presentation;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.newbarams.ajaja.module.plan.application.CreatePlanService;
+import com.newbarams.ajaja.module.plan.application.GetPlanAchieveService;
+
+@AutoConfigureMockMvc
+@WebMvcTest(controllers = PlanController.class)
+class PlanControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private GetPlanAchieveService getPlanAchieveService;
+	@MockBean
+	private CreatePlanService createPlanService;
+
+	@Test
+	@WithMockUser
+	void getPlanAchieve() throws Exception {
+		given(getPlanAchieveService.loadPlanAchieve(anyLong())).willReturn(50);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/plans/feedbacks/1")
+				.with(csrf())
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}
+}
