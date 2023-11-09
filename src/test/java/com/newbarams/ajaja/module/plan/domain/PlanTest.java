a08506f46cb87dd6f952396828cf535a6ae57e82
kys0411 <32180846@dankook.ac.kr>
2023-11-10 01:54:00 +0900

    [AJA-306] Test: Plan 도메인 삭제 테스트 코드 추가

--- src/test/java/com/newbarams/ajaja/module/plan/domain/PlanTest.java
+++ src/test/java/com/newbarams/ajaja/module/plan/domain/PlanTest.java
@@ -1,5 +1,9 @@
 package com.newbarams.ajaja.module.plan.domain;
 
+import static com.newbarams.ajaja.module.plan.exception.ErrorMessage.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
@@ -7,13 +11,41 @@ import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntr
 import com.navercorp.fixturemonkey.jakarta.validation.plugin.JakartaValidationPlugin;
 
 class PlanTest {
+	private final FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
+		.objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
+		.plugin(new JakartaValidationPlugin())
+		.build();
+
 	@Test
 	void createPlan_Success() {
-		FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
-			.objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
-			.plugin(new JakartaValidationPlugin())
-			.build();
-
 		fixtureMonkey.giveMeOne(Plan.class);
+	}
+
+	@Test
+	@DisplayName("요청 날짜가 1월이면 작성한 계획을 삭제할 수 있다.")
+	void deletePlan_Success() {
+		PlanStatus planStatus = new PlanStatus(true);
+
+		Plan plan = fixtureMonkey.giveMeBuilder(Plan.class)
+			.set("status", planStatus)
+			.sample();
+
+		plan.delete("Thu JAN 09 2023");
+
+		assertThat(plan.getStatus().isDeleted()).isEqualTo(true);
+	}
+
+	@Test
+	@DisplayName("요청 날짜가 1월이 아닐 경우 계획을 삭제할 수 없다.")
+	void deletePlan_Fail_By_Date() {
+		PlanStatus planStatus = new PlanStatus(true);
+
+		Plan plan = fixtureMonkey.giveMeBuilder(Plan.class)
+			.set("status", planStatus)
+			.sample();
+
+		assertThatThrownBy(() -> plan.delete("Thu NOV 09 2023"))
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessage(INVALID_UPDATABLE_DATE.getMessage());
 	}
 }
