ed7a0f710f97bee7e97d687e337ca54765ed699e
Hejow <gmlwh124@naver.com>
2023-11-12 18:16:20 +0900

    [AJA-321] Update(.security): apply jwt settings

--- src/main/java/com/newbarams/ajaja/global/security/SecurityConfig.java
+++ src/main/java/com/newbarams/ajaja/global/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.newbarams.ajaja.global.security;
 
 import static org.springframework.security.config.Customizer.*;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,20 +11,36 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.newbarams.ajaja.global.security.jwt.filter.JwtAuthenticationFilter;
+import com.newbarams.ajaja.global.security.jwt.filter.JwtExceptionFilter;
+import com.newbarams.ajaja.global.security.jwt.util.JwtParser;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+	private static final List<String> ALLOW_LIST = List.of("/swagger-ui", "/api-docs", "/login", "/mock/**");
+
+	private final JwtParser jwtParser;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.httpBasic(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable) // todo: check
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
 			.csrf(AbstractHttpConfigurer::disable)
 			.cors(withDefaults())
 			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-		http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
+		http
+			.addFilterBefore(new JwtAuthenticationFilter(ALLOW_LIST, jwtParser),
+				UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
 
 		return http.build();
 	}
