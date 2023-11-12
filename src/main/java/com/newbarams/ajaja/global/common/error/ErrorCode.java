355d420b5a67144133af515922743f3503cb3604
Hejow <gmlwh124@naver.com>
2023-11-12 17:14:25 +0900

    Add(.jwt): jwt parser

--- src/main/java/com/newbarams/ajaja/global/common/error/ErrorCode.java
+++ src/main/java/com/newbarams/ajaja/global/common/error/ErrorCode.java
@@ -13,6 +13,10 @@ public enum ErrorCode {
 	// 400
 	BEAN_VALIDATE_FAIL_EXCEPTION(BAD_REQUEST, "올바르지 않은 데이터입니다."),
 	INVALID_BEARER_FORMAT(BAD_REQUEST, "유효한 Bearer 토큰의 형식이 아닙니다."),
+	INVALID_JWT_SIGNATURE(BAD_REQUEST, "잘못된 서명입니다."),
+	INVALID_JWT(BAD_REQUEST, "잘못된 토큰입니다."),
+	EXPIRED_JWT(BAD_REQUEST, "만료된 토큰입니다."),
+	UNSUPPORTED_JWT(BAD_REQUEST, "지원하지 않는 토큰입니다."),
 
 	// 404
 	USER_NOT_FOUND(NOT_FOUND, "사용자가 존재하지 않습니다."),
---
+++ src/main/java/com/newbarams/ajaja/global/security/jwt/util/JwtParser.java
@@ -0,0 +1,55 @@
+package com.newbarams.ajaja.global.security.jwt.util;
+
+import static com.newbarams.ajaja.global.common.error.ErrorCode.*;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import com.newbarams.ajaja.global.common.exeption.AjajaException;
+import com.newbarams.ajaja.global.security.common.CustomUserDetailService;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+@Component
+public class JwtParser {
+	private final JwtSecretProvider jwtSecretProvider;
+	private final CustomUserDetailService userDetailService;
+	private final io.jsonwebtoken.JwtParser parser;
+
+	public JwtParser(JwtSecretProvider jwtSecretProvider, CustomUserDetailService userDetailService) {
+		this.jwtSecretProvider = jwtSecretProvider;
+		this.userDetailService = userDetailService;
+		this.parser = Jwts.parser().verifyWith(jwtSecretProvider.getSecretKey()).build();
+	}
+
+	public Authentication parseAuthentication(String jwt) {
+		Long userId = parseId(jwt);
+		UserDetails user = userDetailService.loadUserByUsername(String.valueOf(userId)); // todo: ridiculous code
+		return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+	}
+
+	public Long parseId(String jwt) {
+		Claims claims = parseClaim(jwt);
+		return claims.get(jwtSecretProvider.getSignature(), Long.class);
+	}
+
+	private Claims parseClaim(String jwt) {
+		try {
+			return parser.parseSignedClaims(jwt).getPayload();
+		} catch (SecurityException | MalformedJwtException e) {
+			throw new AjajaException(INVALID_JWT_SIGNATURE);
+		} catch (ExpiredJwtException e) {
+			throw new AjajaException(EXPIRED_JWT);
+		} catch (UnsupportedJwtException e) {
+			throw new AjajaException(UNSUPPORTED_JWT);
+		} catch (IllegalArgumentException e) {
+			throw new AjajaException(INVALID_JWT);
+		}
+	}
+}
