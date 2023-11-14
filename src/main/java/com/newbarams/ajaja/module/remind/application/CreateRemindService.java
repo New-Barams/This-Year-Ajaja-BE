4c55bb64664708ae540366f375d84f1ebd6fb5eb
2jie0516 <dhtpgks10044@gmail.com>
2023-11-14 23:41:27 +0900

    [AJA-253] Add(.src): 리마인드 생성 클래스 구현

---
+++ src/main/java/com/newbarams/ajaja/module/remind/application/CreateRemindService.java
@@ -0,0 +1,61 @@
+package com.newbarams.ajaja.module.remind.application;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.newbarams.ajaja.module.plan.domain.Message;
+import com.newbarams.ajaja.module.plan.domain.RemindInfo;
+import com.newbarams.ajaja.module.remind.domain.Info;
+import com.newbarams.ajaja.module.remind.domain.Period;
+import com.newbarams.ajaja.module.remind.domain.Remind;
+import com.newbarams.ajaja.module.remind.repository.RemindRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CreateRemindService {
+	private final RemindRepository remindRepository;
+
+	public void createRemind(Long userId, Long planId, List<Message> messages, RemindInfo remindInfo) {
+		ListIterator<Message> messageIterator = messages.listIterator();
+
+		int totalPeriod = remindInfo.getRemindTotalPeriod();
+		int remindMonth = remindInfo.getRemindTerm() == 1 ? 2 : remindInfo.getRemindTerm();
+
+		while (remindMonth <= totalPeriod) {
+			Info info = new Info(messageIterator.next().getContent());
+			int remindTime = getRemindTime(remindInfo.getRemindTime().name());
+
+			Instant time = parseInstant(remindInfo.getRemindDate(), remindMonth, remindTime);
+
+			Period period = new Period(time, time.plus(31, ChronoUnit.DAYS));
+			Remind remind = Remind.plan(userId, planId, info, period);
+
+			remindRepository.save(remind);
+			remindMonth += remindInfo.getRemindTerm();
+		}
+	}
+
+	private int getRemindTime(String name) {
+		if (name.equals("MORNING")) {
+			return 15;
+		} else if (name.equals("AFTERNOON")) {
+			return 15;
+		} else {
+			return 15;
+		}
+	}
+
+	private Instant parseInstant(int remindDate, int remindMonth, int remindTime) {
+		return Instant.parse(
+			"2024-" + String.format("%02d", remindMonth) + "-" + String.format("%02d", remindDate) + "T"
+				+ remindTime + ":00:00Z");
+	}
+}
