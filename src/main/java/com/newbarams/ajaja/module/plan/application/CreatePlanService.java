42d0d37643d9f34fe1fcf66a3690a0bfebc438b7
2jie0516 <dhtpgks10044@gmail.com>
2023-11-14 23:42:22 +0900

    [AJA-253] Update(.src): 계획 생성 서비스에 리마인드 생성 메소드 추가

--- src/main/java/com/newbarams/ajaja/module/plan/application/CreatePlanService.java
+++ src/main/java/com/newbarams/ajaja/module/plan/application/CreatePlanService.java
@@ -10,6 +10,7 @@ import com.newbarams.ajaja.module.plan.dto.PlanRequest;
 import com.newbarams.ajaja.module.plan.dto.PlanResponse;
 import com.newbarams.ajaja.module.plan.mapper.PlanMapper;
 import com.newbarams.ajaja.module.plan.repository.PlanRepository;
+import com.newbarams.ajaja.module.remind.application.CreateRemindService;
 import com.newbarams.ajaja.module.tag.application.TagService;
 import com.newbarams.ajaja.module.tag.domain.Tag;
 
@@ -21,12 +22,15 @@ import lombok.RequiredArgsConstructor;
 public class CreatePlanService {
 	private final PlanRepository planRepository;
 	private final TagService tagService;
+	private final CreateRemindService createRemindService;
 
 	public PlanResponse.Create create(PlanRequest.Create request) {
 		Set<Tag> tags = tagService.getTags(request.tags());
 
 		Plan plan = PlanMapper.toEntity(request, 1L, tags);
 		Plan savedPlan = planRepository.save(plan);
+
+		createRemindService.createRemind(plan.getUserId(), plan.getId(), plan.getMessages(), plan.getInfo());
 
 		return PlanMapper.toResponse(savedPlan);
 	}
