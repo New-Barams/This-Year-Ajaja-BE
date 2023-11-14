35f96cdb61309da9e1e587f7a365d96f87d66807
Hejow <gmlwh124@naver.com>
2023-11-14 16:13:09 +0900

    [AJA-368] Add(.user): logout

---
+++ src/main/java/com/newbarams/ajaja/module/user/application/LogoutService.java
@@ -0,0 +1,31 @@
+package com.newbarams.ajaja.module.user.application;
+
+import static com.newbarams.ajaja.global.common.error.ErrorCode.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.newbarams.ajaja.global.common.exception.AjajaException;
+import com.newbarams.ajaja.global.security.jwt.util.JwtRemover;
+import com.newbarams.ajaja.module.user.domain.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LogoutService {
+	private final UserRepository userRepository;
+	private final JwtRemover jwtRemover;
+
+	public void logout(Long id) {
+		validateExist(id);
+		jwtRemover.remove(id);
+	}
+
+	private void validateExist(Long id) {
+		if (!userRepository.existsById(id)) {
+			throw new AjajaException(USER_NOT_FOUND);
+		}
+	}
+}
