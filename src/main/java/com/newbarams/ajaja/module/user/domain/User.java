14887b5ebcb60c19154b000820b93711ae9ad740
Hejow <gmlwh124@naver.com>
2023-11-15 12:36:45 +0900

    Add(.user): receive type on user

--- src/main/java/com/newbarams/ajaja/module/user/domain/User.java
+++ src/main/java/com/newbarams/ajaja/module/user/domain/User.java
@@ -7,6 +7,8 @@ import com.newbarams.ajaja.global.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,6 +23,10 @@ import lombok.NoArgsConstructor;
 @Where(clause = "is_deleted = false")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity<User> {
+	enum ReceiveType {
+		KAKAO, EMAIL, BOTH
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
@@ -30,11 +36,15 @@ public class User extends BaseEntity<User> {
 	private Nickname nickname;
 	private Email email;
 
+	@Enumerated(EnumType.STRING)
+	private ReceiveType receiveType;
+
 	private boolean isDeleted;
 
 	public User(String nickname, String email) {
 		this.nickname = new Nickname(nickname);
 		this.email = new Email(email);
+		this.receiveType = ReceiveType.EMAIL;
 		this.isDeleted = false;
 	}
 
@@ -60,5 +70,9 @@ public class User extends BaseEntity<User> {
 
 	public void updateNickname(String nickname) {
 		this.nickname = new Nickname(nickname);
+	}
+
+	public void updateReceive(String receiveType) {
+		this.receiveType = ReceiveType.valueOf(receiveType.toUpperCase());
 	}
 }
