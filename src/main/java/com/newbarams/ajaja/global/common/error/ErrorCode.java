e122057b51b28d6e58d625c104e3711c1d2434a8
Hejow <gmlwh124@naver.com>
2023-11-15 09:20:57 +0900

    [AJA-363] Add(.user): verify certification, delete update remind

--- src/main/java/com/newbarams/ajaja/global/common/error/ErrorCode.java
+++ src/main/java/com/newbarams/ajaja/global/common/error/ErrorCode.java
@@ -20,11 +20,12 @@ public enum ErrorCode {
 	INVALID_JWT(BAD_REQUEST, "잘못된 토큰입니다."),
 	EXPIRED_JWT(BAD_REQUEST, "만료된 토큰입니다."),
 	UNSUPPORTED_JWT(BAD_REQUEST, "지원하지 않는 토큰입니다."),
-	ALREADY_EMAIL_VERIFIED(BAD_REQUEST, "이미 이메일 인증을 진행한 사용자입니다."),
+	CERTIFICATION_NOT_MATCH(BAD_REQUEST, "인증 번호가 일치하지 않습니다."),
 	UNABLE_TO_VERIFY_EMAIL(BAD_REQUEST, "이메일 인증을 할 수 없습니다. 인증이 완료된 상태라면 기존 리마인드 이메일과 다른 이메일을 입력해야 합니다."),
 
 	// 404
 	USER_NOT_FOUND(NOT_FOUND, "사용자가 존재하지 않습니다."),
+	CERTIFICATION_NOT_FOUND(NOT_FOUND, "인증 요청 시간이 초과하였거나 인증을 요청한 적이 없습니다."),
 
 	// 500
 	AJAJA_SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 내부 오류입니다. 관리자에게 문의바랍니다.");
--- src/main/java/com/newbarams/ajaja/module/user/application/SendVerificationEmailService.java
+++ src/main/java/com/newbarams/ajaja/module/user/application/SendVerificationEmailService.java
@@ -1,10 +1,12 @@
 package com.newbarams.ajaja.module.user.application;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.newbarams.ajaja.module.user.domain.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,22 +18,26 @@ public class SendVerificationEmailService {
 	private static final String KEY_PREFIX = "AJAJA ";
 
 	private final SendCertificationService sendCertificationService;
-	private final UpdateRemindEmailService updateRemindEmailService;
+	private final RetrieveUserService retrieveUserService;
 	private final RedisTemplate<String, Object> redisTemplate;
 
-	public void sendVerification(Long id, String email) {
-		updateRemindEmailService.updateIfDifferent(id, email);
+	public void sendVerification(Long userId, String email) {
+		validateEmail(userId, email);
 		String certification = RandomCertificationGenerator.generate();
 		sendCertificationService.send(email, certification);
-		saveVerificationOnCache(email, certification);
+		saveVerificationOnCache(userId, email, certification);
 	}
 
-	private void saveVerificationOnCache(String email, String certification) {
+	private void validateEmail(Long id, String email) {
+		User user = retrieveUserService.loadExistUserById(id);
+		user.validateEmail(email);
+	}
+
+	private void saveVerificationOnCache(Long userId, String email, String certification) {
 		redisTemplate.opsForValue().set(
-			KEY_PREFIX + email,
-			certification,
-			VERIFICATION_EMAIL_EXPIRE_IN,
-			TimeUnit.MILLISECONDS
+			KEY_PREFIX + userId,
+			new EmailVerification(email, certification),
+			Duration.ofMillis(VERIFICATION_EMAIL_EXPIRE_IN)
 		);
 	}
 }
