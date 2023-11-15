577aad5935c12bef0433115bf75039e57c071599
2jie0516 <dhtpgks10044@gmail.com>
2023-11-15 13:44:16 +0900

    [AJA-253] Chore(.src) : 인터페이스에 서비스 어노테이션 추가

--- src/main/java/com/newbarams/ajaja/module/remind/application/SendEmailRemindService.java
+++ src/main/java/com/newbarams/ajaja/module/remind/application/SendEmailRemindService.java
@@ -1,5 +1,8 @@
 package com.newbarams.ajaja.module.remind.application;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface SendEmailRemindService {
 	void send(String email, String message, Long feedbackId);
 }
