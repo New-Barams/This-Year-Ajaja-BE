109d4bb966f39c9ce99c857a2b9dbd312f6c2611
2jie0516 <dhtpgks10044@gmail.com>
2023-11-26 22:01:57 +0900

    [AJA-444] Update(.src) : 시간 value 클래스 코드 수정

--- src/main/java/com/newbarams/ajaja/global/common/TimeValue.java
+++ src/main/java/com/newbarams/ajaja/global/common/TimeValue.java
@@ -1,12 +1,19 @@
 package com.newbarams.ajaja.global.common;
 
+import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public class TimeValue {
-	Instant instant = Instant.now();
-	ZonedDateTime zonedDateTime = instant.atZone(ZoneId.systemDefault());
+	private final Instant instant;
+	private final ZonedDateTime zonedDateTime;
+
+	public TimeValue(Instant instant) {
+		this.instant = instant;
+		zonedDateTime = instant.atZone(ZoneId.of("Asia/Seoul"));
+	}
 
 	public int getMonth() {
 		return zonedDateTime.getMonthValue();
@@ -16,11 +23,11 @@ public class TimeValue {
 		return zonedDateTime.getDayOfMonth();
 	}
 
-	public int getMonthFrom(Instant instant) {
-		return instant.atZone(ZoneId.systemDefault()).getMonthValue();
+	public Timestamp getTimeStampOf() {
+		return Timestamp.valueOf(instant.toString());
 	}
 
-	public int getDateFrom(Instant instant) {
-		return instant.atZone(ZoneId.systemDefault()).getDayOfMonth();
+	public LocalDateTime getLocalDateTimeOf() {
+		return zonedDateTime.toLocalDateTime();
 	}
 }
