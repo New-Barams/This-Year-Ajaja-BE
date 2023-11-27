7221d532f21e8f6ec17f6a977e9e7a16fdbb8d66
2jie0516 <dhtpgks10044@gmail.com>
2023-11-27 22:30:05 +0900

    [AJA-447] Update(.src) : 로깅 응답값 수정 및 실행시간 추가

--- src/main/java/com/newbarams/ajaja/infra/logger/LoggingAspect.java
+++ src/main/java/com/newbarams/ajaja/infra/logger/LoggingAspect.java
@@ -1,20 +1,12 @@
 package com.newbarams.ajaja.infra.logger;
 
-import java.time.Instant;
-import java.util.Enumeration;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import com.newbarams.ajaja.global.common.TimeValue;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -31,47 +23,21 @@ public class LoggingAspect {
 
 	@Around(CONTROLLER_LOGGING_CONDITION)
 	public Object executeLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+		StopWatch stopWatch = new StopWatch();
+
+		stopWatch.start();
 		Object result = joinPoint.proceed();
+		stopWatch.stop();
+
 		ServletRequestAttributes requestAttributes
 			= (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
 		HttpServletRequest request = (requestAttributes).getRequest();
 
-		Map<String, Object> requestParams = createRequestParams(joinPoint, request);
+		String controllerLog = "[API] Called : " + request.getMethod() + " " + request.getRequestURI() +
+			",Processed : " + stopWatch.getTotalTimeMillis() + "ms";
 
-		log.info("request log - {}", requestParams);
+		log.info(controllerLog);
 
 		return result;
-	}
-
-	private static Map<String, Object> createRequestParams(
-		ProceedingJoinPoint joinPoint,
-		HttpServletRequest request
-	) {
-		Map<String, Object> requestParams = new LinkedHashMap<>();
-		TimeValue timeValue = new TimeValue(Instant.now());
-
-		try {
-			requestParams.put("controller", joinPoint.getSignature().getDeclaringType().getName());
-			requestParams.put("methodName", joinPoint.getSignature().getName());
-			requestParams.put("requestParams", getParams(request));
-			requestParams.put("log_time", timeValue.toLocalDateTime());
-			requestParams.put("request_uri", request.getRequestURI());
-			requestParams.put("request_http_method", request.getMethod());
-		} catch (Exception e) {
-			log.error("Controller Request Exception", e);
-		}
-
-		return requestParams;
-	}
-
-	private static JSONObject getParams(HttpServletRequest request) throws JSONException {
-		JSONObject jsonObject = new JSONObject();
-		Enumeration<String> params = request.getParameterNames();
-		while (params.hasMoreElements()) {
-			String param = params.nextElement();
-			String replaceParam = param.replaceAll("\\.", "-");
-			jsonObject.put(replaceParam, request.getParameter(param));
-		}
-		return jsonObject;
 	}
 }
