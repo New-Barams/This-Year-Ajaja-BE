6aed25962fc13d83131df031e33f8a962084c909
2jie0516 <dhtpgks10044@gmail.com>
2023-11-27 16:54:47 +0900

    [AJA-519] Chore(.src) : 사용하지 않는 로깅 어노테이션 제거

--- src/main/java/com/newbarams/ajaja/infra/ses/SesSendAjajaRemindService.java
+++ src/main/java/com/newbarams/ajaja/infra/ses/SesSendAjajaRemindService.java
@@ -7,9 +7,7 @@ import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.newbarams.ajaja.module.ajaja.application.SendAjajaRemindService;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SesSendAjajaRemindService implements SendAjajaRemindService {
