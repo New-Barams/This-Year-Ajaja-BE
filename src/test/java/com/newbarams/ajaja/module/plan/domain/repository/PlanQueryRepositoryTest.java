77787e310d828b372be1ce552d09e6a85bf1e684
kys0411 <32180846@dankook.ac.kr>
2023-11-30 15:29:11 +0900

    [AJA-469] Test: Plan QueryRepository Test Code 추가

--- src/test/java/com/newbarams/ajaja/module/plan/domain/repository/PlanQueryRepositoryTest.java
+++ src/test/java/com/newbarams/ajaja/module/plan/domain/repository/PlanQueryRepositoryTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,12 @@ import com.newbarams.ajaja.module.plan.domain.Content;
 import com.newbarams.ajaja.module.plan.domain.Message;
 import com.newbarams.ajaja.module.plan.domain.Plan;
 import com.newbarams.ajaja.module.plan.domain.PlanStatus;
+import com.newbarams.ajaja.module.plan.dto.PlanRequest;
+import com.newbarams.ajaja.module.plan.dto.PlanResponse;
+import com.newbarams.ajaja.module.user.domain.OauthInfo;
+import com.newbarams.ajaja.module.user.domain.OauthProvider;
+import com.newbarams.ajaja.module.user.domain.User;
+import com.newbarams.ajaja.module.user.domain.repository.UserRepository;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
@@ -45,6 +53,14 @@ class PlanQueryRepositoryTest {
 	@Autowired
 	private PlanRepository planRepository;
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@BeforeEach
+	void deleteAll() {
+		planRepository.deleteAll();
+	}
+
 	@Test
 	@DisplayName("올해의 계획들만 사용자의 아이디로 가져올 수 있어야 한다.")
 	void findAllCurrentPlansByUserId_Success() {
@@ -64,5 +80,98 @@ class PlanQueryRepositoryTest {
 
 		// then
 		assertThat(currentPlans).isNotEmpty().hasSize(expectedSize);
+	}
+
+	@Test
+	@DisplayName("계획의 ID를 이용해 하나의 계획을 가져올 수 있다.")
+	void findById_Success() {
+		User user = new User("nickname", "email@naver.com",
+			new OauthInfo(Arbitraries.longs().greaterOrEqual(0).sample(), OauthProvider.KAKAO));
+
+		User saved = userRepository.save(user);
+
+		Plan plan = monkey.giveMeBuilder(Plan.class)
+			.set("userId", saved.getId())
+			.set("status", new PlanStatus(true))
+			.sample();
+
+		Plan savedPlan = planRepository.save(plan);
+
+		Optional<PlanResponse.GetOne> response = planQueryRepository.findById(savedPlan.getId(), saved.getId());
+		assertThat(response).isNotEmpty();
+	}
+
+	@Test
+	@DisplayName("계획 id가 존재하지 않으면 가져올 수 없다.")
+	void findById_Fail_By_Not_Exist_PlanId() {
+		User user = new User("nickname", "email@naver.com",
+			new OauthInfo(Arbitraries.longs().greaterOrEqual(0).sample(), OauthProvider.KAKAO));
+
+		User saved = userRepository.save(user);
+
+		Plan plan = monkey.giveMeBuilder(Plan.class)
+			.set("userId", saved.getId())
+			.set("status", new PlanStatus(true))
+			.sample();
+
+		planRepository.save(plan);
+
+		Optional<PlanResponse.GetOne> response = planQueryRepository.findById(-1L, saved.getId());
+		assertThat(response).isEmpty();
+	}
+
+	@Test
+	@DisplayName("최신순 or 인기순 정렬 시 지정된 개수만큼의 계획을 받는다.")
+	void findAllByCursorAndSorting_Default_Success() {
+		int pageSize = 3;
+
+		User user = new User("nickname", "email@naver.com",
+			new OauthInfo(Arbitraries.longs().greaterOrEqual(0).sample(), OauthProvider.KAKAO));
+
+		User saved = userRepository.save(user);
+
+		List<Plan> plans = monkey.giveMeBuilder(Plan.class)
+			.set("userId", saved.getId())
+			.set("status", new PlanStatus(true))
+			.sampleList(10);
+
+		planRepository.saveAll(plans);
+
+		PlanRequest.GetAll latestReq = new PlanRequest.GetAll("latest", true, null, null);
+		PlanRequest.GetAll ajajaReq = new PlanRequest.GetAll("ajaja", true, null, null);
+
+		List<PlanResponse.GetAll> latestRes = planQueryRepository.findAllByCursorAndSorting(latestReq);
+		List<PlanResponse.GetAll> ajajaRes = planQueryRepository.findAllByCursorAndSorting(ajajaReq);
+
+		assertThat(latestRes).hasSize(pageSize);
+		assertThat(ajajaRes).hasSize(pageSize);
+	}
+
+	@Test
+	@DisplayName("최신순 정렬 시 아이디 순으로 정렬된 계획들을 반환한다.")
+	void findAllByCursorAndSorting_With_Latest_Condition_Success() {
+		int pageSize = 3;
+
+		User user = new User("nickname", "email@naver.com",
+			new OauthInfo(Arbitraries.longs().greaterOrEqual(0).sample(), OauthProvider.KAKAO));
+
+		User saved = userRepository.save(user);
+
+		for (int i = 0; i < pageSize; i++) {
+			Plan plan = monkey.giveMeBuilder(Plan.class)
+				.set("userId", saved.getId())
+				.set("status", new PlanStatus(true))
+				.sample();
+
+			planRepository.save(plan);
+		}
+
+		PlanRequest.GetAll latestReq = new PlanRequest.GetAll("latest", true, null, null);
+
+		List<PlanResponse.GetAll> latestRes = planQueryRepository.findAllByCursorAndSorting(latestReq);
+
+		for (int i = 0; i < pageSize - 1; i++) {
+			assertThat(latestRes.get(i).id()).isGreaterThan(latestRes.get(i + 1).id());
+		}
 	}
 }
