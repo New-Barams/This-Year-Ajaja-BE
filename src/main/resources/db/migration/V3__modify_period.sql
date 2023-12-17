8026648dcd73ff201fc6741845462947599c1294
2jie0516 <dhtpgks10044@gmail.com>
2023-12-17 13:36:32 +0900

    Chore(.src) : 플라이웨이 버전 업데이트

--- src/main/resources/db/migration/V3__modify_period.sql
+++ src/main/resources/db/migration/V3__modify_period.sql
@@ -3,9 +3,9 @@ ALTER TABLE reminds
     DROP COLUMN starts,
     DROP COLUMN ends,
     ADD COLUMN remind_month int not null,
-    ADD COLUMN remind_date  int not null;
+    ADD COLUMN remind_day   int not null;
 ALTER TABLE remind_messages
     DROP COLUMN message_idx,
     ADD COLUMN remind_month int not null,
-    ADD COLUMN remind_date  int not null;
+    ADD COLUMN remind_day   int not null;
 
