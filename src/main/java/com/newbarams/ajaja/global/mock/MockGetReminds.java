6b5fd3e8bf0877927635971705368a9be4b28553
2jie0516 <dhtpgks10044@gmail.com>
2023-12-25 23:42:52 +0900

    Delete(.src) : 목 컨트롤러 dto 삭제

--- src/main/java/com/newbarams/ajaja/global/mock/MockGetReminds.java
+++
@@ -1,9 +0,0 @@
-package com.newbarams.ajaja.global.mock;
-
-public sealed interface MockGetReminds permits MockGetReminds.Response {
-	record Response(
-		int index,
-		String remindMessage
-	) implements MockGetReminds {
-	}
-}
