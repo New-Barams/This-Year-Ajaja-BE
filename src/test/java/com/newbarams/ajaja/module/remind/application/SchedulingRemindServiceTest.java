d57c9c9ae864b601e0b86a09162596419bdc7ed3
2jie0516 <dhtpgks10044@gmail.com>
2024-01-02 02:46:22 +0900

    Test(.src) : 스케줄링 서비스 코드 테스트 오류 수정

--- src/test/java/com/newbarams/ajaja/module/remind/application/SchedulingRemindServiceTest.java
+++ src/test/java/com/newbarams/ajaja/module/remind/application/SchedulingRemindServiceTest.java
@@ -1,14 +1,20 @@
 package com.newbarams.ajaja.module.remind.application;
 
+import static org.mockito.BDDMockito.*;
+
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import com.newbarams.ajaja.common.support.MockTestSupport;
+import com.newbarams.ajaja.global.common.TimeValue;
+import com.newbarams.ajaja.module.plan.domain.Message;
+import com.newbarams.ajaja.module.plan.domain.Plan;
+import com.newbarams.ajaja.module.plan.domain.RemindInfo;
 import com.newbarams.ajaja.module.plan.infra.PlanQueryRepository;
 import com.newbarams.ajaja.module.remind.application.model.RemindMessageInfo;
 
@@ -22,37 +28,39 @@ class SchedulingRemindServiceTest extends MockTestSupport {
 	private SendPlanRemindService sendPlanRemindService;
 	@Mock
 	private PlanQueryRepository planQueryRepository;
+	@Mock
+	private Plan plan;
 
 	private List<RemindMessageInfo> remindMessage;
 
-	@BeforeEach
-	void setUp() {
-		// List<Message> messages = sut.giveMe(Message.class, 12);
-		// RemindInfo info = sut.giveMeBuilder(RemindInfo.class)
-		// 	.set("remindTerm", 12)
-		// 	.sample();
-		// Plan plan = sut.giveMeBuilder(Plan.class)
-		// 	.set("messages", messages)
-		// 	.set("info", info)
-		// 	.sample();
-		//
-		// remindMessage = sut.giveMeBuilder(RemindMessageInfo.class)
-		// 	.set("plan", plan)
-		// 	.sampleList(5);
-		//
-		// doNothing().when(createRemindService).createRemind(any(), any());
-		// doNothing().when(sendPlanRemindService).send(any(), any(), any(), anyLong());
-		// doNothing().when(createFeedbackService).create(anyLong(), anyLong());
-		// given(planQueryRepository.findAllRemindablePlan(anyString(), any())).willReturn(remindMessage);
-	}
-
 	@Test
-	@DisplayName("리마인드 가능한 계획만큼 리마인드를 보낸다.")
+	@DisplayName("1월을 제외하고 리마인드 가능한 계획만큼 리마인드를 보낸다.")
 	void scheduleMorningRemind_Success_withNoException() {
-		// when
-		// schedulingRemindService.scheduleMorningRemind();
+		// given
+		List<Message> messages = sut.giveMe(Message.class, 11);
+		RemindInfo info = sut.giveMeBuilder(RemindInfo.class)
+			.set("remindTerm", 1)
+			.sample();
+		plan = sut.giveMeBuilder(Plan.class)
+			.set("messages", messages)
+			.set("info", info)
+			.sample();
 
-		// then
-		// then(sendPlanRemindService).should(times(5)).send(any(), any(), any(), anyLong());
+		remindMessage = sut.giveMeBuilder(RemindMessageInfo.class)
+			.set("plan", plan)
+			.sampleList(5);
+
+		given(planQueryRepository.findAllRemindablePlan(anyString(), any())).willReturn(remindMessage);
+
+		// when,then
+		if (new TimeValue().getMonth() == 1) {
+			Assertions.assertThatException().isThrownBy(() -> {
+				schedulingRemindService.scheduleMorningRemind();
+			});
+		} else {
+			schedulingRemindService.scheduleMorningRemind();
+			then(sendPlanRemindService).should(times(5)).send(any(), any(), any(), anyLong());
+		}
+
 	}
 }
