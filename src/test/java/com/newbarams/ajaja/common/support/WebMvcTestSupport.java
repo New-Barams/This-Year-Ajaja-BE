5afbb8b2c54c7267cd0eb78e9b8ac3edbf996f05
Hejow <gmlwh124@naver.com>
2024-01-06 17:38:32 +0900

    Add: rest-docs dependency

--- src/test/java/com/newbarams/ajaja/common/support/WebMvcTestSupport.java
+++ src/test/java/com/newbarams/ajaja/common/support/WebMvcTestSupport.java
@@ -1,14 +1,27 @@
 package com.newbarams.ajaja.common.support;
 
+import static org.apache.commons.codec.CharEncoding.*;
 import static org.springframework.context.annotation.ComponentScan.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.newbarams.ajaja.common.annotation.ApiTest;
@@ -41,7 +54,10 @@ import com.newbarams.ajaja.module.user.application.port.out.GetMyPagePort;
 	excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MockController.class)
 )
 @Import({UserMockBeans.class, AuthMockBeans.class})
+@ExtendWith(RestDocumentationExtension.class)
 public abstract class WebMvcTestSupport extends MonkeySupport {
+	private static final String ANY_END_POINT = "/**";
+
 	protected static final String USER_END_POINT = "/users";
 	protected static final String PLAN_END_POINT = "/plans";
 	protected static final String FEEDBACK_END_POINT = "/feedbacks";
@@ -50,6 +66,23 @@ public abstract class WebMvcTestSupport extends MonkeySupport {
 	protected MockMvc mockMvc;
 	@Autowired
 	protected ObjectMapper objectMapper;
+
+	@BeforeEach
+	void setup(
+		WebApplicationContext webApplicationContext,
+		RestDocumentationContextProvider restDocumentation
+	) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+			.alwaysDo(print())
+			.apply(springSecurity())
+			.apply(documentationConfiguration(restDocumentation))
+			.addFilter(new CharacterEncodingFilter(UTF_8, true))
+			.defaultRequest(post(ANY_END_POINT).with(csrf()))
+			.defaultRequest(get(ANY_END_POINT).with(csrf()))
+			.defaultRequest(put(ANY_END_POINT).with(csrf()))
+			.defaultRequest(delete(ANY_END_POINT).with(csrf()))
+			.build();
+	}
 
 	// JWT
 	@MockBean
