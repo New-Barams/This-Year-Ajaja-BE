ae3330ad12edd1681bee79e3ff76226277b0c307
Hejow <gmlwh124@naver.com>
2024-01-06 18:19:55 +0900

    Delete(.test): unnecessary test config

--- src/test/java/com/newbarams/ajaja/common/support/WebMvcTestSupport.java
+++ src/test/java/com/newbarams/ajaja/common/support/WebMvcTestSupport.java
@@ -14,12 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
@@ -28,7 +26,8 @@ import com.newbarams.ajaja.common.annotation.ApiTest;
 import com.newbarams.ajaja.global.mock.MockController;
 import com.newbarams.ajaja.global.security.jwt.util.JwtParser;
 import com.newbarams.ajaja.module.ajaja.application.SwitchAjajaService;
-import com.newbarams.ajaja.module.auth.application.AuthMockBeans;
+import com.newbarams.ajaja.module.auth.application.port.in.LoginUseCase;
+import com.newbarams.ajaja.module.auth.application.port.in.ReissueTokenUseCase;
 import com.newbarams.ajaja.module.feedback.application.LoadFeedbackInfoService;
 import com.newbarams.ajaja.module.feedback.application.LoadTotalAchieveService;
 import com.newbarams.ajaja.module.feedback.application.UpdateFeedbackService;
@@ -39,21 +38,21 @@ import com.newbarams.ajaja.module.plan.application.LoadPlanService;
 import com.newbarams.ajaja.module.plan.application.UpdatePlanService;
 import com.newbarams.ajaja.module.plan.application.UpdateRemindInfoService;
 import com.newbarams.ajaja.module.remind.application.LoadRemindInfoService;
-import com.newbarams.ajaja.module.user.application.UserMockBeans;
+import com.newbarams.ajaja.module.user.application.port.in.ChangeReceiveTypeUseCase;
+import com.newbarams.ajaja.module.user.application.port.in.LogoutUseCase;
+import com.newbarams.ajaja.module.user.application.port.in.RenewNicknameUseCase;
+import com.newbarams.ajaja.module.user.application.port.in.SendVerificationEmailUseCase;
+import com.newbarams.ajaja.module.user.application.port.in.VerifyCertificationUseCase;
+import com.newbarams.ajaja.module.user.application.port.in.WithdrawUseCase;
 import com.newbarams.ajaja.module.user.application.port.out.GetMyPagePort;
 
 /**
  * Supports Cached Context On WebMvcTest with Monkey <br>
- * Scan All Controllers By Annotation and Manage MockBeans <br>
  * When Authentication is required USE @ApiTest
  * @see ApiTest
  * @author hejow
  */
-@WebMvcTest(
-	includeFilters = @Filter(type = FilterType.ANNOTATION, classes = RestController.class),
-	excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MockController.class)
-)
-@Import({UserMockBeans.class, AuthMockBeans.class})
+@WebMvcTest(excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MockController.class))
 @ExtendWith(RestDocumentationExtension.class)
 public abstract class WebMvcTestSupport extends MonkeySupport {
 	private static final String ANY_END_POINT = "/**";
@@ -84,11 +83,31 @@ public abstract class WebMvcTestSupport extends MonkeySupport {
 			.build();
 	}
 
-	// JWT
+	/**
+	 * Caching Mock Beans
+	 */
 	@MockBean
 	protected JwtParser jwtParser; // todo: delete after authentication aop applied
 
+	// Auth
+	@MockBean
+	protected LoginUseCase loginUseCase;
+	@MockBean
+	protected ReissueTokenUseCase reissueTokenUseCase;
+
 	// User
+	@MockBean
+	protected ChangeReceiveTypeUseCase changeReceiveTypeUseCase;
+	@MockBean
+	protected LogoutUseCase logoutUseCase;
+	@MockBean
+	protected RenewNicknameUseCase renewNicknameUseCase;
+	@MockBean
+	protected SendVerificationEmailUseCase sendVerificationEmailUseCase;
+	@MockBean
+	protected VerifyCertificationUseCase verifyCertificationUseCase;
+	@MockBean
+	protected WithdrawUseCase withdrawUseCase;
 	@MockBean
 	protected GetMyPagePort getMyPagePort;
 
--- src/test/java/com/newbarams/ajaja/module/auth/adapter/in/web/ReissueControllerTest.java
+++ src/test/java/com/newbarams/ajaja/module/auth/adapter/in/web/ReissueControllerTest.java
@@ -1,15 +1,18 @@
 package com.newbarams.ajaja.module.auth.adapter.in.web;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import com.newbarams.ajaja.common.annotation.ApiTest;
 import com.newbarams.ajaja.common.support.WebMvcTestSupport;
 import com.newbarams.ajaja.module.auth.dto.AuthRequest;
+import com.newbarams.ajaja.module.auth.dto.AuthResponse;
 
 class ReissueControllerTest extends WebMvcTestSupport {
 
@@ -17,14 +20,38 @@ class ReissueControllerTest extends WebMvcTestSupport {
 	void reissue_Success() throws Exception {
 		// given
 		AuthRequest.Reissue request = sut.giveMeOne(AuthRequest.Reissue.class);
+		AuthResponse.Token response = sut.giveMeOne(AuthResponse.Token.class);
+
+		given(reissueTokenUseCase.reissue(anyString(), anyString())).willReturn(response);
 
 		// when
-		ResultActions result = mockMvc.perform(post("/reissue")
-				.with(csrf())
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(request)));
+		var result = mockMvc.perform(post("/reissue")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(request)));
 
 		// then
-		result.andExpect(status().isOk());
+		result.andExpectAll(
+			status().isOk(),
+			jsonPath("$.success").value(Boolean.TRUE),
+			jsonPath("$.data.accessToken").value(response.getAccessToken()),
+			jsonPath("$.data.refreshToken").value(response.getRefreshToken()),
+			jsonPath("$.data.accessTokenExpireIn").value(response.getAccessTokenExpireIn())
+		);
+
+		// docs
+		result.andDo(
+			document("reissue token",
+				requestFields(
+					fieldWithPath("accessToken").type(JsonFieldType.STRING).description("엑세스 토큰"),
+					fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("리프레시 토큰")
+				),
+				responseFields(
+					fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공 여부"),
+					fieldWithPath("data.accessToken").type(JsonFieldType.STRING).description("엑세스 토큰"),
+					fieldWithPath("data.refreshToken").type(JsonFieldType.STRING).description("리프레시 토큰"),
+					fieldWithPath("data.accessTokenExpireIn").type(JsonFieldType.NUMBER).description("엑세스 토큰 만료시간")
+				)
+			)
+		);
 	}
 }
