bd7a7b3bd732a2212fdc5c06457b7a690bd2292d
Hejow <gmlwh124@naver.com>
2024-01-10 06:59:01 +0900

    Test(.plan): create test with doc generator

--- src/main/java/com/newbarams/ajaja/module/plan/dto/PlanResponse.java
+++ src/main/java/com/newbarams/ajaja/module/plan/dto/PlanResponse.java
@@ -57,62 +57,43 @@ public final class PlanResponse {
 	@Data
 	public static class GetOne {
 		private final Long id;
-
 		private final Long userId;
 		private final String nickname;
-
 		private final String title;
 		private final String description;
-
 		private final int iconNumber;
-
 		private final boolean isPublic;
 		private final boolean canRemind;
 		private final boolean canAjaja;
-
 		private final long ajajas;
 		private final boolean isPressAjaja;
-
 		private final List<String> tags;
-
 		private final Instant createdAt;
 	}
 
 	@Data
 	public static class GetAll {
 		private final Long id;
-
 		private final Long userId;
 		private final String nickname;
-
 		private final String title;
-
 		private final int iconNumber;
-
 		private final long ajajas;
-
 		private final List<String> tags;
-
 		private final Instant createdAt;
 	}
 
 	@Data
 	public static class Create {
 		private final Long id;
-
 		private final Long userId;
-
 		private final String title;
 		private final String description;
-
 		private final int iconNumber;
-
 		private final boolean isPublic;
 		private final boolean canRemind;
 		private final boolean canAjaja;
-
 		private final int ajajas = 0;
-
 		private final List<String> tags;
 	}
 
--- src/test/java/com/newbarams/ajaja/module/auth/adapter/in/web/ReissueControllerTest.java
+++ src/test/java/com/newbarams/ajaja/module/auth/adapter/in/web/ReissueControllerTest.java
@@ -1,19 +1,14 @@
 package com.newbarams.ajaja.module.auth.adapter.in.web;
 
 import static org.mockito.BDDMockito.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.restdocs.operation.preprocess.Preprocessors;
-import org.springframework.restdocs.payload.JsonFieldType;
 
-import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
-import com.epages.restdocs.apispec.ResourceDocumentation;
-import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.newbarams.ajaja.common.annotation.ApiTest;
 import com.newbarams.ajaja.common.support.WebMvcTestSupport;
+import com.newbarams.ajaja.common.util.DocsGenerator;
 import com.newbarams.ajaja.module.auth.dto.AuthRequest;
 import com.newbarams.ajaja.module.auth.dto.AuthResponse;
 
@@ -43,26 +38,34 @@ class ReissueControllerTest extends WebMvcTestSupport {
 
 		// docs
 		result.andDo(
-			MockMvcRestDocumentationWrapper.document("reissue api",
-				Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
-				Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
-				ResourceDocumentation.resource(ResourceSnippetParameters.builder()
-					.tag("인증 API")
-					.summary("토큰 재발급 API")
-					.description("기존에 발급받은 토큰으로 새로운 토큰을 재발급할 수 있습니다.")
-					.requestFields(
-						fieldWithPath("accessToken").type(JsonFieldType.STRING).description("엑세스 토큰"),
-						fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("리프레시 토큰")
-					)
-					.responseFields(
-						fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공 여부"),
-						fieldWithPath("data.accessToken").type(JsonFieldType.STRING).description("엑세스 토큰"),
-						fieldWithPath("data.refreshToken").type(JsonFieldType.STRING).description("리프레시 토큰"),
-						fieldWithPath("data.accessTokenExpireIn").type(JsonFieldType.NUMBER).description("엑세스 토큰 만료시간")
-					)
-					.build()
-				)
+			DocsGenerator.generate(
+				"인증 API",
+				"토큰 재발급",
+				"토큰 재발급 API",
+				"기존에 발급받은 토큰으로 새로운 토큰을 재발급할 수 있습니다.",
+				false,
+				result
 			)
+			// MockMvcRestDocumentationWrapper.document("reissue api",
+			// 	Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+			// 	Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+			// 	ResourceDocumentation.resource(ResourceSnippetParameters.builder()
+			// 		.tag("인증 API")
+			// 		.summary("토큰 재발급 API")
+			// 		.description("기존에 발급받은 토큰으로 새로운 토큰을 재발급할 수 있습니다.")
+			// 		.requestFields(
+			// 			fieldWithPath("accessToken").type(JsonFieldType.STRING).description("엑세스 토큰"),
+			// 			fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("리프레시 토큰")
+			// 		)
+			// 		.responseFields(
+			// 			fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공 여부"),
+			// 			fieldWithPath("data.accessToken").type(JsonFieldType.STRING).description("엑세스 토큰"),
+			// 			fieldWithPath("data.refreshToken").type(JsonFieldType.STRING).description("리프레시 토큰"),
+			// 			fieldWithPath("data.accessTokenExpireIn").type(JsonFieldType.NUMBER).description("엑세스 토큰 만료시간")
+			// 		)
+			// 		.build()
+			// 	)
+			// )
 		);
 	}
 }
