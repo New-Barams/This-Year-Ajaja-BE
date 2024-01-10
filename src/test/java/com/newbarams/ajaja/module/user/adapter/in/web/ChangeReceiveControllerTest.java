4a1449b61c940907bc549eea2e0e2cf24524f025
Hejow <gmlwh124@naver.com>
2024-01-10 23:43:27 +0900

    Test(.user): apply rest docs

--- src/test/java/com/newbarams/ajaja/module/user/adapter/in/web/ChangeReceiveControllerTest.java
+++ src/test/java/com/newbarams/ajaja/module/user/adapter/in/web/ChangeReceiveControllerTest.java
@@ -1,14 +1,17 @@
 package com.newbarams.ajaja.module.user.adapter.in.web;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
 
 import com.newbarams.ajaja.common.annotation.ApiTest;
 import com.newbarams.ajaja.common.support.WebMvcTestSupport;
+import com.newbarams.ajaja.common.util.ApiTag;
+import com.newbarams.ajaja.common.util.RestDocument;
+import com.newbarams.ajaja.module.user.domain.User;
 import com.newbarams.ajaja.module.user.dto.UserRequest;
 
 class ChangeReceiveControllerTest extends WebMvcTestSupport {
@@ -16,15 +19,31 @@ class ChangeReceiveControllerTest extends WebMvcTestSupport {
 	@ApiTest
 	void changeReceiveType_Success() throws Exception {
 		// given
-		UserRequest.Receive request = sut.giveMeOne(UserRequest.Receive.class);
+		UserRequest.Receive request = new UserRequest.Receive(User.ReceiveType.BOTH);
+
+		willDoNothing().given(changeReceiveTypeUseCase).change(anyLong(), any());
 
 		// when
-		ResultActions result = mockMvc.perform(put(USER_END_POINT + "/receive")
-			.with(csrf())
+		var result = mockMvc.perform(put(USER_END_POINT + "/receive")
+			.header(HttpHeaders.AUTHORIZATION, BEARER_TOKEN)
 			.contentType(MediaType.APPLICATION_JSON)
 			.content(objectMapper.writeValueAsString(request)));
 
 		// then
-		result.andExpect(status().isOk());
+		result.andExpectAll(
+			status().isOk()
+		);
+
+		// docs
+		result.andDo(
+			RestDocument.builder()
+				.identifier("update-receive-type")
+				.tag(ApiTag.USER)
+				.summary("수신 방법 변경 API")
+				.description("리마인드를 수신할 방법을 변경합니다.")
+				.secured(true)
+				.result(result)
+				.generateDocs()
+		);
 	}
 }
--- src/test/java/com/newbarams/ajaja/module/user/adapter/in/web/GetMyPageControllerTest.java
+++ src/test/java/com/newbarams/ajaja/module/user/adapter/in/web/GetMyPageControllerTest.java
@@ -1,27 +1,54 @@
 package com.newbarams.ajaja.module.user.adapter.in.web;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
 
 import com.newbarams.ajaja.common.annotation.ApiTest;
 import com.newbarams.ajaja.common.support.WebMvcTestSupport;
+import com.newbarams.ajaja.common.util.ApiTag;
+import com.newbarams.ajaja.common.util.RestDocument;
+import com.newbarams.ajaja.module.user.dto.UserResponse;
 
 class GetMyPageControllerTest extends WebMvcTestSupport {
 
 	@ApiTest
 	void getMyPage_Success() throws Exception {
 		// given
+		UserResponse.MyPage response
+			= new UserResponse.MyPage("공부하는 돼지", "ajaja@me.com", "ajaja@me.com", true, "KAKAO");
+
+		given(getMyPagePort.findUserInfoById(anyLong())).willReturn(response);
 
 		// when
-		ResultActions result = mockMvc.perform(get(USER_END_POINT)
-			.with(csrf())
+		var result = mockMvc.perform(get(USER_END_POINT)
+			.header(HttpHeaders.AUTHORIZATION, BEARER_TOKEN)
 			.contentType(MediaType.APPLICATION_JSON));
 
 		// then
-		result.andExpect(status().isOk());
+		result.andExpectAll(
+			status().isOk(),
+			jsonPath("$.success").value(Boolean.TRUE),
+			jsonPath("$.data.nickname").value(response.getNickname()),
+			jsonPath("$.data.defaultEmail").value(response.getDefaultEmail()),
+			jsonPath("$.data.remindEmail").value(response.getRemindEmail()),
+			jsonPath("$.data.emailVerified").value(response.isEmailVerified()),
+			jsonPath("$.data.receiveType").value(response.getReceiveType())
+		);
+
+		// docs
+		result.andDo(
+			RestDocument.builder()
+				.identifier("get-my-page")
+				.tag(ApiTag.USER)
+				.summary("마이페이지 API")
+				.description("마이페이지에서 사용될 사용자의 정보를 불러옵니다.")
+				.secured(true)
+				.result(result)
+				.generateDocs()
+		);
 	}
 }
