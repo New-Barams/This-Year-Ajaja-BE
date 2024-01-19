46bb28728f42efdcbc50099e0c5cf0d53490d2e6
Hejow <gmlwh124@naver.com>
2024-01-20 03:04:41 +0900

    Test(.user): get my page test

--- src/main/java/com/newbarams/ajaja/module/user/application/port/out/GetMyPageQuery.java
+++ src/main/java/com/newbarams/ajaja/module/user/application/port/out/GetMyPageQuery.java
@@ -4,9 +4,9 @@ import com.newbarams.ajaja.module.user.dto.UserResponse;
 
 public interface GetMyPageQuery {
 	/**
-	 * Query port can be used on in coming adapter
-	 * @param id target to query
-	 * @return
+	 * Inquiry user profile, Query port can be used on in coming adapter
+	 * @param id target to inquiry
+	 * @return User profile
 	 */
-	UserResponse.MyPage findUserInfoById(Long id); // todo: separate dto
+	UserResponse.MyPage findUserInfoById(Long id);
 }
