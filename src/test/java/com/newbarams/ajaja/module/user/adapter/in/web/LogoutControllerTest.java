3596c745cb7a1ff871486fbcafdaada179e889c1
Hejow <gmlwh124@naver.com>
2024-01-20 02:59:41 +0900

    Test(.user): logout test

--- src/test/java/com/newbarams/ajaja/module/user/adapter/in/web/LogoutControllerTest.java
+++ src/test/java/com/newbarams/ajaja/module/user/adapter/in/web/LogoutControllerTest.java
@@ -1,24 +1,35 @@
 package com.newbarams.ajaja.module.user.adapter.in.web;
 
+import static com.newbarams.ajaja.global.exception.ErrorCode.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
 import com.newbarams.ajaja.common.annotation.ApiTest;
+import com.newbarams.ajaja.common.annotation.ParameterizedApiTest;
 import com.newbarams.ajaja.common.support.WebMvcTestSupport;
 import com.newbarams.ajaja.common.util.ApiTag;
 import com.newbarams.ajaja.common.util.RestDocument;
+import com.newbarams.ajaja.global.exception.AjajaException;
+import com.newbarams.ajaja.global.exception.ErrorCode;
 
 class LogoutControllerTest extends WebMvcTestSupport {
+	private static final String LOGOUT_END_POINT = USER_END_POINT + "/logout";
 
 	@ApiTest
+	@DisplayName("유효한 토큰으로 로그아웃 요청을 보내면 성공한다.")
 	void logout_Success() throws Exception {
 		// given
 
 		// when
-		var result = mockMvc.perform(post(USER_END_POINT + "/logout")
+		var result = mockMvc.perform(post(LOGOUT_END_POINT)
 			.header(HttpHeaders.AUTHORIZATION, BEARER_TOKEN)
 			.contentType(MediaType.APPLICATION_JSON));
 
@@ -32,6 +43,71 @@ class LogoutControllerTest extends WebMvcTestSupport {
 				.tag(ApiTag.USER)
 				.summary("로그아웃 API")
 				.description("발급된 사용자의 토큰을 만료시킵니다.")
+				.secured(true)
+				.result(result)
+				.generateDocs()
+		);
+	}
+
+	@ParameterizedApiTest
+	@MethodSource("authenticationResults")
+	@DisplayName("요청 시 인증에 실패하면 400에러를 반환한다.")
+	void logout_Fail_ByAuthentication(ErrorCode errorCode, String identifier) throws Exception {
+		// given
+		RuntimeException authenticationFailed = new AjajaException(errorCode);
+
+		willThrow(authenticationFailed).given(logoutUseCase).logout(anyLong());
+
+		// when
+		var result = mockMvc.perform(post(LOGOUT_END_POINT)
+			.header(HttpHeaders.AUTHORIZATION, BEARER_TOKEN)
+			.contentType(MediaType.APPLICATION_JSON));
+
+		// then
+		result.andExpectAll(
+			status().isBadRequest(),
+			jsonPath("$.httpStatus").value(BAD_REQUEST.name()),
+			jsonPath("$.errorName").value(errorCode.name()),
+			jsonPath("$.errorMessage").value(errorCode.getMessage())
+		);
+
+		// docs
+		result.andDo(
+			RestDocument.builder()
+				.identifier("logout-fail-" + identifier)
+				.tag(ApiTag.USER)
+				.secured(true)
+				.result(result)
+				.generateDocs()
+		);
+	}
+
+	@ApiTest
+	@DisplayName("존재하지 않는 회원으로 요청하면 404에러를 반환한다.")
+	void logout_Fail_ByNotExistUser() throws Exception {
+		// given
+		RuntimeException useNotFound = new AjajaException(USER_NOT_FOUND);
+
+		willThrow(useNotFound).given(logoutUseCase).logout(anyLong());
+
+		// when
+		var result = mockMvc.perform(post(LOGOUT_END_POINT)
+			.header(HttpHeaders.AUTHORIZATION, BEARER_TOKEN)
+			.contentType(MediaType.APPLICATION_JSON));
+
+		// then
+		result.andExpectAll(
+			status().isNotFound(),
+			jsonPath("$.httpStatus").value(NOT_FOUND.name()),
+			jsonPath("$.errorName").value(USER_NOT_FOUND.name()),
+			jsonPath("$.errorMessage").value(USER_NOT_FOUND.getMessage())
+		);
+
+		// docs
+		result.andDo(
+			RestDocument.builder()
+				.identifier("logout-fail-not-exist-user")
+				.tag(ApiTag.USER)
 				.secured(true)
 				.result(result)
 				.generateDocs()
