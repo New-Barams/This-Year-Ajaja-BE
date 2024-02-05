4b00b9da323403e4822491d265b5f773180e093d
kys0411 <32180846@dankook.ac.kr>
2024-02-05 20:19:37 +0900

    Chore: 중복 메서드 삭제

--- src/main/java/me/ajaja/module/plan/application/LoadPlanDetailService.java
+++ src/main/java/me/ajaja/module/plan/application/LoadPlanDetailService.java
@@ -7,36 +7,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import me.ajaja.global.exception.AjajaException;
-import me.ajaja.module.feedback.application.model.PlanFeedbackInfo;
 import me.ajaja.module.plan.application.port.in.LoadPlanDetailUseCase;
 import me.ajaja.module.plan.application.port.out.FindPlanDetailPort;
-import me.ajaja.module.plan.domain.Plan;
 import me.ajaja.module.plan.dto.PlanResponse;
-import me.ajaja.module.plan.mapper.PlanMapper;
-import me.ajaja.module.remind.application.port.out.FindTargetRemindQuery;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 class LoadPlanDetailService implements LoadPlanDetailUseCase {
 	private final FindPlanDetailPort findPlanDetailPort;
-	private final FindTargetRemindQuery findTargetRemindQuery;
-	private final PlanMapper planMapper;
 
 	@Override
 	public PlanResponse.Detail loadByIdAndOptionalUser(Long userId, Long id) {
 		return findPlanDetailPort.findPlanDetailByIdAndOptionalUser(userId, id)
 			.orElseThrow(() -> AjajaException.withId(id, NOT_FOUND_PLAN));
-	}
-
-	@Override
-	public Plan loadByUserIdAndPlanId(Long userId, Long id) {
-		return findTargetRemindQuery.loadByUserIdAndPlanId(userId, id);
-	}
-
-	@Override
-	public PlanFeedbackInfo loadPlanFeedbackInfoByPlanId(Long userId, Long planId) {
-		Plan plan = loadByUserIdAndPlanId(userId, planId);
-		return planMapper.toModel(plan);
 	}
 }
--- src/main/java/me/ajaja/module/plan/application/port/in/LoadPlanDetailUseCase.java
+++ src/main/java/me/ajaja/module/plan/application/port/in/LoadPlanDetailUseCase.java
@@ -1,13 +1,7 @@
 package me.ajaja.module.plan.application.port.in;
 
-import me.ajaja.module.feedback.application.model.PlanFeedbackInfo;
-import me.ajaja.module.plan.domain.Plan;
 import me.ajaja.module.plan.dto.PlanResponse;
 
 public interface LoadPlanDetailUseCase {
 	PlanResponse.Detail loadByIdAndOptionalUser(Long userId, Long id);
-
-	Plan loadByUserIdAndPlanId(Long userId, Long id);
-
-	PlanFeedbackInfo loadPlanFeedbackInfoByPlanId(Long userId, Long planId);
 }
