76addb9f8e365625a2767fecadc1eb24b520927c
juwoong kim <wwong7247@gmail.com>
2024-02-12 00:05:01 +0900

    Add: 발자취 도메인 생성자 파라미터 DTO 생성

---
+++ src/main/java/me/ajaja/module/footprint/dto/FootprintParam.java
@@ -0,0 +1,27 @@
+package me.ajaja.module.footprint.dto;
+
+import java.util.List;
+import java.util.Set;
+
+import lombok.Data;
+import me.ajaja.module.footprint.domain.Ajaja;
+import me.ajaja.module.footprint.domain.Footprint;
+import me.ajaja.module.footprint.domain.Tag;
+import me.ajaja.module.footprint.domain.TargetPlan;
+import me.ajaja.module.footprint.domain.Title;
+import me.ajaja.module.footprint.domain.Writer;
+
+public final class FootprintParam {
+	@Data
+	public static class Create {
+		private final TargetPlan targetPlan;
+		private final Writer writer;
+
+		private Title title;
+		private Footprint.Type type;
+		private Footprint.Visibility visibility;
+
+		private Set<Tag> tags;
+		private List<Ajaja> ajajas;
+	}
+}
