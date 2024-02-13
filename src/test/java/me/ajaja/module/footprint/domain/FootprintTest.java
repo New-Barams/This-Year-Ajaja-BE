7d9f1c11c3227a7c40e4d7ef253eb194253dcbb6
juwoong kim <wwong7247@gmail.com>
2024-02-13 17:52:19 +0900

    Test: 발자취 도메인 테스트 코드 수정

--- src/test/java/me/ajaja/module/footprint/domain/FootprintTest.java
+++ src/test/java/me/ajaja/module/footprint/domain/FootprintTest.java
@@ -1,13 +1,13 @@
 package me.ajaja.module.footprint.domain;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
-
-import java.sql.Blob;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import jakarta.validation.ConstraintViolationException;
 import me.ajaja.common.support.MonkeySupport;
 import me.ajaja.module.footprint.dto.FootprintParam;
 
@@ -23,34 +23,41 @@ class FootprintTest extends MonkeySupport {
 		.build();
 
 	@Nested
-	@DisplayName("유형별 발자취 도메인 생성을 성공한다")
+	@DisplayName("조건에 맞는 입력 값에 따라 유형별 발자취 도메인 생성에 성공 한다.")
 	class CreateFootprintTest {
 		@Test
-		@DisplayName("자유 형식 발자취 도메인 생성을 성공한다.")
-		public void createFreeFootprint_Success_WithNoException() {
-			assertThatNoException().isThrownBy(
-				() -> {
-					FootprintParam.Create createParam = fixtureMonkey.giveMeOne(FootprintParam.Create.class);
-					Blob content = fixtureMonkey.giveMeOne(Blob.class);
+		@DisplayName("자유 형식 발자취 도메인 생성에 성공 한다.")
+		void create_FreeFootprint_Success_WithNoException() {
+			FootprintParam.Create param = fixtureMonkey.giveMeOne(FootprintParam.Create.class);
+			String content = "content";
 
-					FreeFootprint freeFootprint = new FreeFootprint(createParam, content);
-				}
-			);
+			assertThatNoException().isThrownBy(() -> {
+				FootprintFactory.createFreeFootprint(param, content);
+			});
 		}
 
 		@Test
-		@DisplayName("KPT 형식 발자취 도메인 생성을 성공한다.")
-		public void createKptFootprint_Success_WithNoException() {
-			assertThatNoException().isThrownBy(
-				() -> {
-					FootprintParam.Create createParam = fixtureMonkey.giveMeOne(FootprintParam.Create.class);
-					Blob keepContent = fixtureMonkey.giveMeOne(Blob.class);
-					Blob problemContent = fixtureMonkey.giveMeOne(Blob.class);
-					Blob tryContent = fixtureMonkey.giveMeOne(Blob.class);
+		@DisplayName("KPT 형식 발자취 도메인 생성에 성공 한다.")
+		void create_KptFootprint_Success_WithNoException() {
+			FootprintParam.Create param = fixtureMonkey.giveMeOne(FootprintParam.Create.class);
+			String keepContent = "keepContent";
+			String problemContent = "problemContent";
+			String tryContent = "tryContent";
 
-					KptFootprint kptFootprint = new KptFootprint(createParam, keepContent, problemContent, tryContent);
-				}
-			);
+			assertThatNoException().isThrownBy(() -> {
+				FootprintFactory.createkptFootprint(param, keepContent, problemContent, tryContent);
+			});
 		}
+	}
+
+	@Test
+	@DisplayName("발자취 항목에 대한 값이 null 일 때 예외가 발생 한다.")
+	void create_Fail_ByNoneContnets() {
+		FootprintParam.Create param = fixtureMonkey.giveMeOne(FootprintParam.Create.class);
+		String content = null;
+
+		assertThatExceptionOfType(ConstraintViolationException.class).isThrownBy(
+			() -> FootprintFactory.createFreeFootprint(param, content)
+		);
 	}
 }
