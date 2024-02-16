874339b632dc34c027ea5fd8f615f1acdbb65890
Hejow <gmlwh124@naver.com>
2024-02-16 22:40:10 +0900

    Add(.test): custom assertion

---
+++ src/test/java/me/ajaja/common/extenstion/AssertExtension.java
@@ -0,0 +1,30 @@
+package me.ajaja.common.extenstion;
+
+import org.assertj.core.api.ThrowableAssert;
+import org.assertj.core.api.ThrowableAssertAlternative;
+import org.assertj.core.api.ThrowableTypeAssert;
+
+import me.ajaja.global.exception.AjajaException;
+import me.ajaja.global.exception.ErrorCode;
+
+public class AssertExtension {
+
+	public static CustomThrowableTypeAssert assertThatAjajaException(ErrorCode errorCode) {
+		return new CustomThrowableTypeAssert(AjajaException.class, errorCode);
+	}
+
+	public static class CustomThrowableTypeAssert extends ThrowableTypeAssert<AjajaException> {
+		private final ErrorCode errorCode;
+
+		public CustomThrowableTypeAssert(Class<? extends AjajaException> throwableType, ErrorCode errorCode) {
+			super(throwableType);
+			this.errorCode = errorCode;
+		}
+
+		@Override
+		public ThrowableAssertAlternative<AjajaException> isThrownBy(ThrowableAssert.ThrowingCallable throwingCallable) {
+			return super.isThrownBy(throwingCallable)
+				.withMessage(errorCode.getMessage());
+		}
+	}
+}
