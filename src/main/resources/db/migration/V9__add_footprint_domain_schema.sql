5863b20dcdea7bdd02ccf61a96a44ab8fa6ed3ef
juwoong kim <wwong7247@gmail.com>
2024-02-17 13:03:08 +0900

    Update: 발자취 도메인 DDL sql 스크립트 수정

--- src/main/resources/db/migration/V9__add_footprint_domain_schema.sql
+++ src/main/resources/db/migration/V9__add_footprint_domain_schema.sql
@@ -1,24 +1,8 @@
-CREATE TABLE IF NOT EXISTS footprint_ajajas
-(
-    footprint_id BIGINT NOT NULL,
-    id           bigint NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS footprint_tags
-(
-    footprint_id BIGINT      NOT NULL,
-    id           bigint      NOT NULL,
-    name         VARCHAR(10) NOT NULL,
-    PRIMARY KEY (footprint_id, id, name)
-);
-
 CREATE TABLE IF NOT EXISTS footprints
 (
     footprint_id    BIGINT PRIMARY KEY AUTO_INCREMENT,
     target_id       BIGINT            NOT NULL,
-    target_title    VARCHAR(50)       NOT NULL,
     writer_id       BIGINT            NOT NULL,
-    nickname        VARCHAR(20)       NOT NULL,
     title           VARCHAR(50)       NOT NULL,
     footprint_type  VARCHAR(10)       NOT NULL,
     visible         BIT               NOT NULL,
@@ -30,13 +14,3 @@ CREATE TABLE IF NOT EXISTS footprints
     created_at      TIMESTAMP(6)      NOT NULL,
     updated_at      TIMESTAMP(6)      NOT NULL
 );
-
-ALTER TABLE footprint_ajajas
-    ADD CONSTRAINT FK_footprint_ajajas
-        FOREIGN KEY (footprint_id)
-            REFERENCES footprints (footprint_id);
-
-ALTER TABLE footprint_tags
-    ADD CONSTRAINT FK_footprint_tags
-        FOREIGN KEY (footprint_id)
-            REFERENCES footprints (footprint_id);
