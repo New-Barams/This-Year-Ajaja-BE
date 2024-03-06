86ee12c28b907b0c366b3baa5334627144cf5007
juwoong kim <wwong7247@gmail.com>
2024-03-06 16:44:23 +0900

    Update: 발자취 태그 생성 기능 수정

--- src/main/java/me/ajaja/module/footprint/adapter/out/tag/CreateFootprintTagsAdapter.java
+++ src/main/java/me/ajaja/module/footprint/adapter/out/tag/CreateFootprintTagsAdapter.java
@@ -1,7 +1,5 @@
 package me.ajaja.module.footprint.adapter.out.tag;
 
-import static me.ajaja.module.tag.application.port.in.CreateTagsUseCase.Type.*;
-
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -9,16 +7,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import me.ajaja.module.footprint.application.port.out.CreateTagsPort;
-import me.ajaja.module.tag.application.port.in.CreateTagsUseCase;
+import me.ajaja.module.tag.application.CreateFootprintTagService;
 
 @Component
 @Transactional
 @RequiredArgsConstructor
 public class CreateFootprintTagsAdapter implements CreateTagsPort {
-	private final CreateTagsUseCase createTagUseCase;
+	private final CreateFootprintTagService createTagService;
 
 	@Override
 	public void create(Long footprintId, List<String> tags) {
-		createTagUseCase.create(FOOTPRINT, footprintId, tags);
+		createTagService.create(footprintId, tags);
 	}
 }
---
+++ src/main/java/me/ajaja/module/tag/application/CreateFootprintTagService.java
@@ -0,0 +1,39 @@
+package me.ajaja.module.tag.application;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import me.ajaja.module.tag.domain.FootprintTag;
+import me.ajaja.module.tag.domain.FootprintTagRepository;
+import me.ajaja.module.tag.domain.Tag;
+import me.ajaja.module.tag.domain.TagRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CreateFootprintTagService {
+	private final TagRepository tagRepository;
+	private final FootprintTagRepository footprintTagRepository;
+
+	public void create(Long targetId, List<String> tagNames) {
+		List<Tag> tags = tagNames.stream()
+			.distinct()
+			.map(this::getOrCreateIfNotExists)
+			.toList();
+
+		saveFootprintTag(targetId, tags);
+	}
+
+	private Tag getOrCreateIfNotExists(String name) {
+		return tagRepository.findByName(name).orElseGet(() -> tagRepository.save(new Tag(name)));
+	}
+
+	private void saveFootprintTag(Long footprintId, List<Tag> tags) {
+		for (Tag tag : tags) {
+			footprintTagRepository.save(new FootprintTag(footprintId, tag.getId()));
+		}
+	}
+}
